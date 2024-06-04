f53cf2e83d90811a69ed12cccf7133703e077dee
Fernando Caemerer | Quartile <fernando.caemerer@quartile.com>
2024-06-03 21:06:41 -0300

    first commit

---
+++ scripts/getdates.js
@@ -0,0 +1,10 @@
+// select the DOM elements for output
+const year = document.querySelector("#currentyear");
+
+// use the date object
+const today = new Date();
+
+year.innerHTML = `teseeee`;
+
+
+let oLastModif = new Date(document.lastModified);
